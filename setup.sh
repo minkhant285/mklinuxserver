b511532a0545357b20dc181abcfea3a479054538
Min Khant T Kyi <bryyan.mk@gmail.com>
2019-09-02 05:30:11 +0000

    added setup mode

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir /home/$USER/share
+mkdir /home/$USER/share/hdd1
+mkdir /home/$USER/share/hdd2
+mkdir /home/$USER/share/hdd3
+mkdir /home/$USER/share/hdd4
+
+sudo apt update
+sudo apt install samba
+sudo apt install vsftpd
+sudo apt install tightvncserver
+sudo apt install minidlna
